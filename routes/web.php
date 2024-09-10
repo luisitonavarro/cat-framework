42095f9ae7af4050dface8a525526141e8f72923
Luis Navarro <rdam_azul@hotmail.com>
2024-09-10 15:04:15 -0600

    Actualizando rutas

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Routing\Router;
 
 // Define rutas
-$router->get('/', function () {
+$router->get('/', 'App\Core\Interfaces\Controllers\HomeController@index');
+
+$router->get('/home', function () {
     return 'Bienvenido a Cat Framework!';
 });
 
-$router->get('home', 'App\Core\Interfaces\Controllers\HomeController@index');
+
+
